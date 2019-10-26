4114fac995a5ddf08ddc050f0d8c3edfc282097d
Mirza K <kabiljagic@student.tugraz.at>
2019-10-26 17:29:06 +0200

    random with syscall

--- tls/random.cpp
+++ tls/random.cpp
@@ -1,7 +1,10 @@
 #include "random.h"
+#include <linux/random.h>
+
+#include <unistd.h>
+#include <sys/syscall.h>
 
 bool get_random_data(uint8_t* data, std::size_t size)
 {
-  /// \todo Fetch size random bytes and store them in data.
-  return false;
+    syscall(SYS_getrandom, data, size, firstIndex);
 }
