632c77be1250ebc49a210ee6008e7efbaf1620fc
Mirza K <kabiljagic@student.tugraz.at>
2020-01-12 18:22:03 +0100

    tests

--- pir/tests/task3_custom.cpp
+++ pir/tests/task3_custom.cpp
@@ -1,6 +1,9 @@
 #include "../../utils/tests.h"
 #include <check.h>
+#include "../paillier.h"
+#include "../bigint/BigInteger.h"
 
+using namespace std;
 // Include your custom testcases here
 // Write at LEAST 10 custom tests to verify your own implementation.
 //
@@ -29,32 +32,307 @@
 
 namespace
 {
-  // include the constants you need for your test here
-  // const std::string test_string = "its_2018";
+    // include the constants you need for your test here
+    // const std::string test_string = "its_2018";
+    auto as = BigInteger1024::FromFile(SOURCE_DIR "data/as1024.txt");
+    auto bs = BigInteger1024::FromFile(SOURCE_DIR "data/bs1024.txt");
+    auto zero_1024 = bs[0];
+    auto max_1024 = bs[2];
+    const BigInteger1024 one_1024 = BigInteger1024(1);
 }
 
-// Sample to illustrate test:
+//START_TEST(lcm){
+//        ck_assert_bi_eq(paillier::lcm(as[0],bs[0]), zero_1024);
+//    }
+//END_TEST
 //
-// START_TEST(custom_1){
-//   std::string part1 = "its";
-//   std::string part2 = "_2018";
-//   std::string to_check = part1 + part2;
-//   ck_assert_str_eq(test_string.c_str(), to_check.c_str());
-// }
-// END_TEST
+//START_TEST(gcd){
+//        BigInteger1024 a = BigInteger1024(10);
+//        BigInteger1024 b = BigInteger1024(2);
+//        ck_assert_bi_eq(paillier::gcd(a,b), BigInteger1024(10));
+//    }
+//END_TEST
+//
+//START_TEST(not_equals_empty){
+//        printf("Testing not_equals_empty:\n");
+//        ck_assert_int_eq(paillier::not_equal(BigInteger1024(),BigInteger1024()),false);
+//    }
+//END_TEST
+//
+//START_TEST(not_equals_zero){
+//        printf("Testing not_equals_zero:\n");
+//        ck_assert_int_eq(paillier::not_equal(BigInteger1024(0),BigInteger1024(0)), false);
+//    }
+//END_TEST
+
+START_TEST(addition_empty_1024){
+        auto res = BigInteger1024() + BigInteger1024();
+        ck_assert_bi_eq(res, BigInteger1024());
+    }
+END_TEST
+
+START_TEST(addition_zero_1024){
+        auto res = BigInteger1024(0) + BigInteger1024(0);
+        ck_assert_bi_eq(res, BigInteger1024(0));
+    }
+END_TEST
+
+START_TEST(addition_empty_2048){
+        auto res = BigInteger2048() + BigInteger2048();
+        ck_assert_bi_eq(res, BigInteger2048());
+    }
+END_TEST
+
+START_TEST(addition_zero_2048){
+        auto res = BigInteger2048(0) + BigInteger2048(0);
+        ck_assert_bi_eq(res, BigInteger2048(0));
+    }
+END_TEST
+
+START_TEST(addition_zero_2048_1024){
+        auto res = BigInteger2048(0) + BigInteger1024(0);
+        ck_assert_bi_eq(res, BigInteger2048(0));
+    }
+END_TEST
+
+START_TEST(addition_zero_1024_2048){
+        auto res = BigInteger1024(0) + BigInteger2048(0);
+        ck_assert_bi_eq(res, BigInteger2048(0));
+    }
+END_TEST
+
+START_TEST(addition_empty_2048_1024){
+        auto res = BigInteger2048() + BigInteger1024();
+        ck_assert_bi_eq(res, BigInteger2048());
+    }
+END_TEST
+
+START_TEST(addition_empty_1024_2048){
+        auto res = BigInteger1024() + BigInteger2048();
+        ck_assert_bi_eq(res, BigInteger2048());
+    }
+END_TEST
+
+START_TEST(subtraction_empty_1024){
+        auto res = BigInteger1024() - BigInteger1024();
+        ck_assert_bi_eq(res, BigInteger1024());
+    }
+END_TEST
+
+START_TEST(subtraction_zero_1024){
+        auto res = BigInteger1024(0) - BigInteger1024(0);
+        ck_assert_bi_eq(res, BigInteger1024(0));
+    }
+END_TEST
+
+START_TEST(subtraction_empty_2048){
+        auto res = BigInteger2048() - BigInteger2048();
+        ck_assert_bi_eq(res, BigInteger2048());
+    }
+END_TEST
+
+START_TEST(subtraction_zero_2048){
+        auto res = BigInteger2048(0) - BigInteger2048(0);
+        ck_assert_bi_eq(res, BigInteger2048(0));
+    }
+END_TEST
+
+START_TEST(subtraction_zero_2048_1024){
+        auto res = BigInteger2048(0) - BigInteger1024(0);
+        ck_assert_bi_eq(res, BigInteger2048(0));
+    }
+END_TEST
+
+START_TEST(subtraction_zero_1024_2048){
+        auto res = BigInteger1024(0) - BigInteger2048(0);
+        ck_assert_bi_eq(res, BigInteger2048(0));
+    }
+END_TEST
+
+START_TEST(subtraction_empty_2048_1024){
+        auto res = BigInteger2048() - BigInteger1024();
+        ck_assert_bi_eq(res, BigInteger2048());
+    }
+END_TEST
+
+START_TEST(subtraction_empty_1024_2048){
+        auto res = BigInteger1024() - BigInteger2048();
+        ck_assert_bi_eq(res, BigInteger2048());
+    }
+END_TEST
+
+START_TEST(multiplication_empty_1024){
+        auto res = BigInteger1024() * BigInteger1024();
+        ck_assert_bi_eq(res, BigInteger1024());
+    }
+END_TEST
+
+START_TEST(multiplication_zero_1024){
+        auto res = BigInteger1024(0) * BigInteger1024(0);
+        ck_assert_bi_eq(res, BigInteger1024(0));
+    }
+END_TEST
+
+START_TEST(multiplication_empty_2048){
+        auto res = BigInteger2048() * BigInteger2048();
+        ck_assert_bi_eq(res, BigInteger2048());
+    }
+END_TEST
+
+START_TEST(multiplication_zero_2048){
+        auto res = BigInteger2048(0) * BigInteger2048(0);
+        ck_assert_bi_eq(res, BigInteger2048(0));
+    }
+END_TEST
+
+START_TEST(multiplication_zero_2048_1024){
+        auto res = BigInteger2048(0) * BigInteger1024(0);
+        ck_assert_bi_eq(res, BigInteger2048(0));
+    }
+END_TEST
+
+START_TEST(multiplication_zero_1024_2048){
+        auto res = BigInteger1024(0) * BigInteger2048(0);
+        ck_assert_bi_eq(res, BigInteger2048(0));
+    }
+END_TEST
+
+START_TEST(multiplication_empty_2048_1024){
+        auto res = BigInteger2048() * BigInteger1024();
+        ck_assert_bi_eq(res, BigInteger2048());
+    }
+END_TEST
+
+START_TEST(multiplication_empty_1024_2048){
+        auto res = BigInteger1024() * BigInteger2048();
+        ck_assert_bi_eq(res, BigInteger2048());
+    }
+END_TEST
+
+START_TEST(division_empty_2048_1024){
+        auto res = BigInteger2048() / BigInteger1024();
+        ck_assert_bi_eq(res, BigInteger1024());
+    }
+END_TEST
+
+START_TEST(division_zero_2048_1024){
+        auto res = BigInteger2048(0) / BigInteger1024(0);
+        ck_assert_bi_eq(res, BigInteger1024(0));
+    }
+END_TEST
+
+START_TEST(zero_greater_than_1024){
+        const BigInteger1024 a = BigInteger1024(0);
+        const BigInteger1024 b = BigInteger1024(0);
+        ck_assert_int_eq(BigInteger::GreaterThan((word*)a.GetData(), (word*)b.GetData(), NUM_WORDS_1024, NUM_WORDS_1024), false);
+    }
+END_TEST
+START_TEST(empty_greater_than_1024){
+        const BigInteger1024 a = BigInteger1024();
+        const BigInteger1024 b = BigInteger1024();
+        ck_assert_int_eq(BigInteger::GreaterThan((word*)a.GetData(), (word*)b.GetData(), NUM_WORDS_1024, NUM_WORDS_1024), false);
+    }
+END_TEST
+
+START_TEST(zero_greater_than_2048){
+        const BigInteger2048 a = BigInteger2048(0);
+        const BigInteger2048 b = BigInteger2048(0);
+        ck_assert_int_eq(BigInteger::GreaterThan((word*)a.GetData(), (word*)b.GetData(), NUM_WORDS_2048, NUM_WORDS_2048), false);
+    }
+END_TEST
+
+START_TEST(empty_greater_than_2048){
+        const BigInteger2048 a = BigInteger2048();
+        const BigInteger2048 b = BigInteger2048();
+        ck_assert_int_eq(BigInteger::GreaterThan((word*)a.GetData(), (word*)b.GetData(), NUM_WORDS_2048, NUM_WORDS_2048), false);
+    }
+END_TEST
+
+
+START_TEST(zero_smaller_than_1024){
+        const BigInteger1024 a = BigInteger1024(0);
+        const BigInteger1024 b = BigInteger1024(0);
+        ck_assert_int_eq(BigInteger::SmallerThan((word*)a.GetData(), (word*)b.GetData(), NUM_WORDS_1024, NUM_WORDS_1024), false);
+    }
+END_TEST
+START_TEST(empty_smaller_than_1024){
+        const BigInteger1024 a = BigInteger1024();
+        const BigInteger1024 b = BigInteger1024();
+        ck_assert_int_eq(BigInteger::SmallerThan((word*)a.GetData(), (word*)b.GetData(), NUM_WORDS_1024, NUM_WORDS_1024), false);
+    }
+END_TEST
+
+START_TEST(zero_smaller_than_2048){
+        const BigInteger2048 a = BigInteger2048(0);
+        const BigInteger2048 b = BigInteger2048(0);
+        ck_assert_int_eq(BigInteger::SmallerThan((word*)a.GetData(), (word*)b.GetData(), NUM_WORDS_2048, NUM_WORDS_2048), false);
+    }
+END_TEST
+
+START_TEST(empty_smaller_than_2048){
+        const BigInteger2048 a = BigInteger2048();
+        const BigInteger2048 b = BigInteger2048();
+        ck_assert_int_eq(BigInteger::SmallerThan((word*)a.GetData(), (word*)b.GetData(), NUM_WORDS_2048, NUM_WORDS_2048), false);
+    }
+END_TEST
+
 
 int main(int argc, char** argv)
 {
-  Suite* suite = suite_create("Student Task 2 Tests");
-  // TCase* tcase = tcase_create("FIRST");
-  // tcase_add_test(tcase, custom_1);
-  // suite_add_tcase(suite, tcase);
+    Suite* suite = suite_create("Student Task 3 Tests");
 
-  SRunner* suite_runner = srunner_create(suite);
-  srunner_run(suite_runner, argc, argv);
+    //TCase* tcase_paillier = tcase_create("Paillier");
+//   tcase_add_test(tcase_paillier, lcm);
+  //  tcase_add_test(tcase_paillier, gcd);
+  //  tcase_add_test(tcase_paillier, not_equals_zero);
+   // tcase_add_test(tcase_paillier, not_equals_empty);
+    //suite_add_tcase(suite, tcase_paillier);
 
-  int number_failed = srunner_ntests_failed(suite_runner);
-  srunner_free(suite_runner);
+    TCase* tcase_bigInt = tcase_create("BigInt");
+    tcase_add_test(tcase_bigInt, addition_empty_1024);
+    tcase_add_test(tcase_bigInt, addition_zero_1024);
+    tcase_add_test(tcase_bigInt, addition_empty_2048);
+    tcase_add_test(tcase_bigInt, addition_zero_2048);
+    tcase_add_test(tcase_bigInt, addition_zero_2048_1024);
+    tcase_add_test(tcase_bigInt, addition_zero_1024_2048);
+    tcase_add_test(tcase_bigInt, addition_empty_2048_1024);
+    tcase_add_test(tcase_bigInt, addition_empty_1024_2048);
+    tcase_add_test(tcase_bigInt, subtraction_empty_1024);
+    tcase_add_test(tcase_bigInt, subtraction_zero_1024);
+    tcase_add_test(tcase_bigInt, subtraction_empty_2048);
+    tcase_add_test(tcase_bigInt, subtraction_zero_2048);
+    tcase_add_test(tcase_bigInt, subtraction_zero_2048_1024);
+    tcase_add_test(tcase_bigInt, subtraction_zero_1024_2048);
+    tcase_add_test(tcase_bigInt, subtraction_empty_2048_1024);
+    tcase_add_test(tcase_bigInt, subtraction_empty_1024_2048);
+    tcase_add_test(tcase_bigInt, multiplication_empty_1024);
+    tcase_add_test(tcase_bigInt, multiplication_zero_1024);
+    tcase_add_test(tcase_bigInt, multiplication_empty_2048);
+    tcase_add_test(tcase_bigInt, multiplication_zero_2048);
+    tcase_add_test(tcase_bigInt, multiplication_zero_2048_1024);
+    tcase_add_test(tcase_bigInt, multiplication_zero_1024_2048);
+    tcase_add_test(tcase_bigInt, multiplication_empty_2048_1024);
+    tcase_add_test(tcase_bigInt, multiplication_empty_1024_2048);
+    //tcase_add_test(tcase_bigInt, division_empty_2048_1024);
+    //tcase_add_test(tcase_bigInt, division_zero_2048_1024);
+    tcase_add_test(tcase_bigInt, zero_greater_than_1024);
+    tcase_add_test(tcase_bigInt, empty_greater_than_1024);
+    //tcase_add_test(tcase_bigInt, zero_greater_than_2048);
+    //tcase_add_test(tcase_bigInt, empty_greater_than_2048);
+    tcase_add_test(tcase_bigInt, zero_smaller_than_1024);
+    tcase_add_test(tcase_bigInt, empty_smaller_than_1024);
+    //tcase_add_test(tcase_bigInt, zero_smaller_than_2048);
+    //tcase_add_test(tcase_bigInt, empty_smaller_than_2048);
 
-  return !number_failed ? EXIT_SUCCESS : EXIT_FAILURE;
+    suite_add_tcase(suite, tcase_bigInt);
+
+
+
+    SRunner* suite_runner = srunner_create(suite);
+    srunner_run(suite_runner, argc, argv);
+
+    int number_failed = srunner_ntests_failed(suite_runner);
+    srunner_free(suite_runner);
+
+    return !number_failed ? EXIT_SUCCESS : EXIT_FAILURE;
 }
